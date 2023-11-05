3f63acdc7f669cc45397a607bd8f8ebeade4381b
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-11-05 10:46:21 -0300

    base

---
+++ frontend/agua-limpa/src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+      <div>
+        <h1>lista não ordenada </h1>
+        
+      </div>
+      <div>
+        <ul>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+          <li>test </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
