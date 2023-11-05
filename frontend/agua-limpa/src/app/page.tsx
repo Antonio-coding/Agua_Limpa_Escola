fcc2f70dfc505ed03ebad3028f4282fea6de4e81
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-11-05 11:19:19 -0300

    Update page.tsx

--- frontend/agua-limpa/src/app/page.tsx
+++ frontend/agua-limpa/src/app/page.tsx
@@ -3,6 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <nav>
+        <div>
+          
+        </div>
+        <div>
+          <ul>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>home</li>
+            <li>Pesquisar</li>
+          </ul>
+        </div>
+      </nav>
       <div>
         <h1>Hello World</h1>
       </div>
